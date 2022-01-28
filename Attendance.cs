05ec2859c7016afb5db5044f805882d274346032
SantoshThakare <santoshthakare50@gmail.com>
2022-01-28 19:47:09 +0530

    [santosh]: Folder created

---
+++ Attendance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class Attendance
+    {
+        const int EMP_PRESENT = 0, EMP_ABSENT = 1;
+        public void check()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == EMP_ABSENT)
+            {
+                Console.WriteLine("Emploee is Absent");
+
+            }
+            if (empCheck == EMP_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+
+
+
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeWageComputation;
+
+
+Console.WriteLine("Welcome to EmployeeWegeComputation");
+
+    Attendance attendance = new Attendance();
+    attendance.check();
