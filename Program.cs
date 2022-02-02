a64a9df2c08fb0f45996ec31da42065c2bacb023
SantoshThakare <santoshthakare50@gmail.com>
2022-02-02 15:16:28 +0530

    [santosh ]: Added Dailly Employee Wage

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeWageComputation;
+
+Console.WriteLine("Hello, World!");
+
+
+WageEmp emp = new WageEmp();
+emp.Wage(); 
---
+++ WageEmp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class WageEmp
+    {
+        public void Wage()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HR = 20;
+            int emHrs = 0;
+            int emWage = 0;
+            Random random = new Random();
+            int empCheck = random .Next(0 , 2);
+            if (empCheck == IS_FULL_TIME )
+            {
+                emHrs = 8;
+            }
+            else
+            {
+                emHrs = 0;
+            }
+            emWage = emHrs * IS_FULL_TIME;
+            Console.WriteLine("Employee Wage is :" + emWage);
+
+        }
+    }
+}
