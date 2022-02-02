5aaf91b94647106d6a71d1a1031ba2ed7cc67f53
SantoshThakare <santoshthakare50@gmail.com>
2022-02-02 15:27:50 +0530

    [santosh ]: Added Part Time Wage

--- WageEmp.cs
+++ WageEmp.cs
@@ -10,13 +10,18 @@ namespace EmployeeWageComputation
     {
         public void Wage()
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HR = 20;
             int emHrs = 0;
             int emWage = 0;
             Random random = new Random();
-            int empCheck = random .Next(0 , 2);
-            if (empCheck == IS_FULL_TIME )
+            int empCheck = random .Next(0 , 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                emHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 emHrs = 8;
             }
@@ -24,7 +29,7 @@ namespace EmployeeWageComputation
             {
                 emHrs = 0;
             }
-            emWage = emHrs * IS_FULL_TIME;
+            emWage = emHrs * EMP_RATE_PER_HR;
             Console.WriteLine("Employee Wage is :" + emWage);
 
         }
